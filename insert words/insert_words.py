9dddb723e30837053375f1c5c929a45f4190584e
Melanie Lum <32724195+mellumfluous@users.noreply.github.com>
2020-02-04 22:25:28 -0800

    made some changes, but works the same way

--- insert words/insert_words.py
+++ insert words/insert_words.py
@@ -1,25 +1,33 @@
-import re, os, sys
+import re
+import os
+import sys
+import traceback
 
 # Use python3, not python2
 # To run: "py.exe insert_words.py" or "./insert_words.py"
 
-def ree():
+def insert_words():
+
+    # Try to get and open the file to change
     try:
-        FileName = input("Please type a file name: \n")
-        originalFile = open(FileName)
-        originalContent = originalFile.read()
-        originalFile.close()
-    except Exception as e:
-        print("Error: " +e)
+        file_name = input("Please type a file name: \n")
+        with open(file_name) as opened_file:
+            content = opened_file.read()
+    except (IOError, EOFError, FileNotFoundError) as e:
+        print(e)
+        traceback.print_exc()
         sys.exit()
 
-    braketWords = re.search(r'{([a-z A-z]*)}', originalContent)
+    # Look through the file for brackets to replace and prompt the user
+    # for what to replace it with
+    brackets = re.search(r'{([a-z A-z]*)}', content)
     company = ""
-    while braketWords:
-        replaceWith = input("insert " + braketWords.group(1) +": ")
-        originalContent = re.sub(braketWords.group(), replaceWith, originalContent)
-        braketWords = re.search(r'{([a-z A-z]*)}', originalContent)
-    newFile = open(FileName +" - changed.txt", 'w')
-    newFile.write(originalContent)
-    newFile.close()
-ree()
+    while brackets:
+        replace = input("insert " + brackets.group(1) +": ")
+        content = re.sub(brackets.group(), replace, content)
+        brackets = re.search(r'{([a-z A-z]*)}', content)
+    # Add " - changed.txt" to the original file prefix and write the content
+    prefix = re.search(r'(.*).(?<=(\.))(.*)', file_name)
+    with open("{} - changed.txt".format(prefix.group(1)), 'w') as new_file:
+        new_file.write(content)
+insert_words()
